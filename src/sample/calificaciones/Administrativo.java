dbd0f79f0c6131fd5a7b8a2c1601d686312f5e0d
Alan de Jesus Ramirez Valencia <48893881+AlanDeJesusRamirezValencia@users.noreply.github.com>
2021-03-06 21:44:34 -0600

    Login Añadido, Cambios en las carpetas

--- src/sample/calificaciones/Administrativo.java
+++ src/sample/calificaciones/Administrativo.java
@@ -1,4 +1,4 @@
-package sample;
+package sample.calificaciones;
 
 public class Administrativo {
 
---
+++ src/sample/ui/Login.java
@@ -0,0 +1,48 @@
+package sample.ui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Login {
+    @FXML
+    private TextField usuario;
+
+    @FXML
+    private PasswordField contraseña;
+
+    @FXML
+    private Label datosIncorrectos;
+
+
+    public void iniciarSesion(javafx.event.ActionEvent actionEvent) {
+        if(validarCampos(usuario.getText(), contraseña.getText())){
+            try { irACalificaciones(); }
+            catch (IOException e) {
+                e.printStackTrace();
+                e.getCause();
+            }
+        }
+        else datosIncorrectos.setText("*Usuario y/o Contraseña incorrectos");
+    }
+
+    private void irACalificaciones() throws IOException {
+        Parent nuevaEscena = FXMLLoader.load(getClass().getResource("../recursos/escenarios/Calificaciones.fxml"));
+        Stage stageActual = (Stage) contraseña.getScene().getWindow();
+        stageActual.setTitle("Calificaciones");
+        stageActual.setScene(new Scene(nuevaEscena, 800, 600));
+        stageActual.show();
+    }
+
+    private boolean validarCampos(String usuario, String contraseña) {
+        //Validar el usuario y contraseña segun la base de datos
+        return true;
+    }
+}
